8877fc0c68677d77d2238ab1bc1d1333afda3a38
Sam <samuel.pelletier@hotmail.fr>
2018-03-20 08:50:17 +0100

    Fix delete route

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 function displayCheckDelete(id){
     var answer = confirm("Souhaitez-vous vraiment supprimer l'utilisateur ?")
     if(answer){
-        window.location.replace("../admin/users/delete/"+id);
+        window.location.replace(((window.location.href).substring(0,(window.location.href).search('/public/')+8))+"admin/users/delete/"+id); //Creation URL with base URL
     }
 }
