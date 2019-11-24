f8bc7a0ba3e2c7c1d57681c14299c165ae22c2b5
Yves Rogez <yves.rogez@rgz.ovh>
2019-11-24 12:41:30 +0100

    Auth : The 'benevole' access with least user rights is set by default at user creation (instead of 'admin').

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
-        $user->assignRole('admin');
+        $user->assignRole('benevole');
         return $user;
     }
 }
